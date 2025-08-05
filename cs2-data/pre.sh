22b6a3c83b42f2c280f8dcd0f49193c6de5a9045
caiomayan <caiomayan@icloud.com>
2025-08-04 21:54:29 -0300

    feat: uploading first time, initial opium data cs2 docker

---
+++ cs2-data/pre.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# PRE HOOK
+#  Make your customisation here
+echo "pre-hook: noop"
+echo "PATCHING gameinfo.ini FOR METAMOD FIX..."
+bash /home/steam/cs2-dedicated/acmrs.sh
+echo "METAMOD PATCH COMPLETE"
